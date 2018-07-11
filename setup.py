f8869cc78d68f4e2b58ca48127313b138bdb8ef8
Markus Kötter <koetter@luis.uni-hannover.de>
2018-07-11 08:21:28 +0200

    setup.py - update data

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='acltk',
-      version='0.1',
-      description='Cisco ACL parsing and filtering tookit',
+      version='0.2',
+      description='Cisco, OPNSense, pfSense ACL processing toolkit',
       author='Markus Koetter',
-      author_email='koetter@rrzn.uni-hannover.de',
+      author_email='koetter@luis.uni-hannover.de',
       url='..',
       packages=['acltk'],
       package_dir = {'acltk': 'lib'},
-      requires=['grako']
+      requires=['tatsu','jinja2']
 )
